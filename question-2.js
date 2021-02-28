d72b25b2fb284ee668d95f09b92f6c91c62dce6f
TonyNett <71494210+TonyNett@users.noreply.github.com>
2021-02-28 16:06:51 +0100

    Create question-2.js

---
+++ question-2.js
@@ -0,0 +1,15 @@
+// QUESTION 2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+async function getData() {
+  const response = await fetch(url);
+
+  const answer = await response.json();
+
+  console.log(answer);
+}
+
+
+
+getData();
