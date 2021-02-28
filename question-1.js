0cdba011f810946b0843d50bbb960389621971b8
TonyNett <71494210+TonyNett@users.noreply.github.com>
2021-02-28 16:05:04 +0100

    Create question-1.js

---
+++ question-1.js
@@ -0,0 +1,15 @@
+// QUESTION 1
+
+// function division(a, b) {
+//     return a % b;
+// }
+
+// const division = (a, b) => {
+//   return a % b;
+// };
+
+const division = (a, b) => a % b;
+
+
+const calculation = division(3, 3);
+console.log(calculation); 
