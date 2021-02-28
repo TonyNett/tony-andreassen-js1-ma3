20486a8f3986bbd6ddd5a6112aa5a0c41fa002cc
TonyNett <71494210+TonyNett@users.noreply.github.com>
2021-02-28 19:56:41 +0100

    console log html, and tags.length

--- js/question-2.js
+++ js/question-2.js
@@ -2,6 +2,8 @@
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
+const displayContainer = document.querySelector(".display");
+
 async function getData() {
   
   const response = await fetch(url);
@@ -10,9 +12,28 @@ async function getData() {
 
   const facts = answer.results;
 
+  displayContainer.innerHTML = "";
+
+  let html = "";
+
   for(let i = 0; i < facts.length; i++) {
-    console.log(facts[i].name);
+    
+    html += `${facts[i].name}
+    ${facts[i].rating}
+    ${facts[i].tags.length}
+    `
+            
+    
+    console.log(html);
+
+    if (i === 7) {
+      break;
+    }
+
+    displayContainer.innerHTML = html;
   }
+
+
 
 
 }
